e9bd7dc889992e15fa449e66b2cb558a33a52a4e
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2026-01-10 03:21:36 +0100

    Refactor courses routes with CourseLayout
    Updated the routing structure to nest CoursesPage and CourseDetailsPage under a new CourseLayout. Also moved course-related pages to the 'pages/course' directory for better organization.

--- course-app/src/App.jsx
+++ course-app/src/App.jsx
@@ -4,10 +4,11 @@ import MainLayout from "./layouts/MainLayout.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import AboutPage from "./pages/AboutPage.jsx";
 import ContactPage from "./pages/help/ContactPage.jsx";
-import CoursesPage, {coursesLoader} from "./pages/CoursesPage.jsx";
+import CoursesPage, {coursesLoader} from "./pages/course/CoursesPage.jsx";
 import HelpLayout from "./layouts/HelpLayput.jsx";
 import FaqPage from "./pages/help/FaqPage.jsx";
-import CourseDetailsPage, {courseDetailsLoader} from "./pages/CourseDetailsPage.jsx";
+import CourseDetailsPage, {courseDetailsLoader} from "./pages/course/CourseDetailsPage.jsx";
+import CourseLayout from "./layouts/CourseLayout.jsx";
 
 
 const router = createBrowserRouter([
@@ -18,8 +19,14 @@ const router = createBrowserRouter([
             {index: true, element: <HomePage/>},
             {path: "home", element: <HomePage/>},
             {path: "about", element: <AboutPage/>},
-            {path: "courses", element: <CoursesPage/>, loader: coursesLoader},
-            {path: "courses/:id", element: <CourseDetailsPage/>, loader:courseDetailsLoader},
+            {
+                path: "courses",
+                element: <CourseLayout/>,
+                children: [
+                    {index: true, element: <CoursesPage/>, loader:coursesLoader},
+                    {path: ":id", element: <CourseDetailsPage/>, loader:courseDetailsLoader},
+                ]
+            },
             {
                 path: "help",
                 element: <HelpLayout/>,
