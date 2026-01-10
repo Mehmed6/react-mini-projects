88a705faa40f7fe4b16eddf8ad09aac645145c4a
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2026-01-10 01:48:04 +0100

    Add AboutPage component
    Introduces the AboutPage component with placeholder content for the About section.

---
+++ course-app/src/pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+export default function AboutPage() {
+    return (
+        <>
+            <div id="about">
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Dignissimos dolorem earum error eveniet ipsum neque provident
+                    quae veritatis vero voluptas?
+                </p>
+            </div>
+        </>
+    )
+}
