6a05bfb8cd89950545596207c1d087637969a642
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2026-01-10 01:47:50 +0100

    Add HomePage component
    Add HomePage component with heading and placeholder text

---
+++ course-app/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+    return (
+        <>
+            <div id="home">
+                <h1>Home</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Dignissimos dolorem earum error eveniet ipsum neque provident
+                    quae veritatis vero voluptas?
+                </p>
+            </div>
+        </>
+    )
+}
