5f2f437cc45785f86bd61b658d0cacab6e314a5a
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2026-01-10 04:45:34 +0100

    Add 'New Course' link to CourseLayout
    Replaced placeholder text with a 'New Course' link in the CourseLayout component to provide navigation for creating a new course.

--- course-app/src/layouts/CourseLayout.jsx
+++ course-app/src/layouts/CourseLayout.jsx
@@ -1,12 +1,11 @@
-import {Outlet} from "react-router";
+import {Link, Outlet} from "react-router";
 
 export default function CourseLayout() {
     return (
         <div id="course-layout">
             <h1>Course List</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur
-                et expedita fugit illum labore non numquam quae rerum. Accusantium
-                earum esse id in laboriosam magnam molestias neque perspiciatis placeat voluptas?
+            <p>
+                <Link to="create">New Course</Link>
             </p>
             <Outlet/>
         </div>
