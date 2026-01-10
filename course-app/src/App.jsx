01f5f773369ba64950c097d663f2af420ffa9ea5
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2026-01-10 01:50:52 +0100

    Add routing with react-router and main layouts
    Integrated react-router with a browser router and set up main and help layouts. Added routes for home, about, courses, contact, and FAQ pages, replacing the static App content.

--- course-app/src/App.jsx
+++ course-app/src/App.jsx
@@ -1,11 +1,37 @@
-
 import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router";
+import MainLayout from "./layouts/MainLayout.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import ContactPage from "./pages/help/ContactPage.jsx";
+import CoursesPage from "./pages/CoursesPage.jsx";
+import HelpLayout from "./layouts/HelpLayput.jsx";
+import FaqPage from "./pages/help/FaqPage.jsx";
 
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout/>,
+        children: [
+            {index: true, element: <HomePage/>},
+            {path: "home", element: <HomePage/>},
+            {path: "about", element: <AboutPage/>},
+            {path: "courses", element: <CoursesPage/>},
+            {
+                path: "help",
+                element: <HelpLayout/>,
+                children: [
+                    {path: "contact", element: <ContactPage/>},
+                    {path: "faq", element: <FaqPage/>},
+                ]
+            }
+        ]
+    }
+])
 function App() {
 
-  return (
-      <h1>App</h1>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
