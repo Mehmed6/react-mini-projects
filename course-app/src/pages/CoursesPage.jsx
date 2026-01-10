e95e5934bf950037e4b31fa0cb5353cab383824d
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2026-01-10 01:48:17 +0100

    Add CoursesPage component
    Introduces a new CoursesPage component displaying a heading and placeholder text for the courses section.

---
+++ course-app/src/pages/CoursesPage.jsx
@@ -0,0 +1,13 @@
+export default function CoursesPage() {
+    return (
+        <>
+            <div id="courses">
+                <h1>Courses</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Dignissimos dolorem earum error eveniet ipsum neque provident
+                    quae veritatis vero voluptas?
+                </p>
+            </div>
+        </>
+    )
+}
