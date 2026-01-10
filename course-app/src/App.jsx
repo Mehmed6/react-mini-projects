3c088f7b0b152ef4c71760c994eae400b19ee2b8
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2026-01-10 05:47:03 +0100

    Add edit, create, and delete routes for courses
    Introduces nested routes for editing, creating, and deleting courses. Adds CourseEditPage and CourseCreatePage components, and connects appropriate actions and loaders to support these operations.

--- course-app/src/App.jsx
+++ course-app/src/App.jsx
@@ -4,11 +4,14 @@ import MainLayout from "./layouts/MainLayout.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import AboutPage from "./pages/AboutPage.jsx";
 import ContactPage from "./pages/help/ContactPage.jsx";
-import CoursesPage, {coursesLoader} from "./pages/course/CoursesPage.jsx";
+import CoursesPage, {courseDeleteAction, coursesLoader} from "./pages/course/CoursesPage.jsx";
 import HelpLayout from "./layouts/HelpLayput.jsx";
 import FaqPage from "./pages/help/FaqPage.jsx";
 import CourseDetailsPage, {courseDetailsLoader} from "./pages/course/CourseDetailsPage.jsx";
 import CourseLayout from "./layouts/CourseLayout.jsx";
+import CourseEditPage from "./pages/course/CourseEditPage.jsx";
+import CourseCreatePage from "./pages/course/CourseCreatePage.jsx";
+import {courseAction} from "./pages/course/CourseForm.jsx";
 
 
 const router = createBrowserRouter([
@@ -24,7 +27,17 @@ const router = createBrowserRouter([
                 element: <CourseLayout/>,
                 children: [
                     {index: true, element: <CoursesPage/>, loader:coursesLoader},
-                    {path: ":id", element: <CourseDetailsPage/>, loader:courseDetailsLoader},
+                    {
+                        id: "course-details",
+                        path: ":id",
+                        loader: courseDetailsLoader,
+                        children: [
+                            {index: true, element: <CourseDetailsPage/>},
+                            {path: "edit", element: <CourseEditPage/>, action:courseAction},
+                            {path: "delete", action:courseDeleteAction}
+                        ]
+                    },
+                    {path: "create", element: <CourseCreatePage/>, action:courseAction},
                 ]
             },
             {
