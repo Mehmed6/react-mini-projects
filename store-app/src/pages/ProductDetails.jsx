a7cf036235cf8d47b7b1af953f19840e76af7cde
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2026-01-13 05:34:40 +0100

    Integrate cart context and API in ProductDetails
    Refactored ProductDetailsPage to use the cart context and centralized API client for product details and cart operations. Added add-to-cart functionality, loading state for add action, and passed relevant props to ProductItem.

--- store-app/src/pages/ProductDetails.jsx
+++ store-app/src/pages/ProductDetails.jsx
@@ -2,17 +2,32 @@ import {useParams} from "react-router";
 import {useEffect, useState} from "react";
 import ProductItem from "../components/ProductItem.jsx";
 import Loading from "../components/Loading.jsx";
+import requests from "../api/apiClient.js";
+import {useCartContext} from "../context/CartContext.jsx";
 
 export default function ProductDetailsPage() {
     const {id} = useParams();
     const [product, setProduct] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [isAdding, setIsAdding] = useState(false);
+    const {cart, setCart} = useCartContext();
+
+    const cartItem = cart?.cartItems.find(item => item.product.productId === product?.id);
+
+    function handleAddItem(productId) {
+        setIsAdding(true);
+
+        requests.cart.addItem(productId)
+            .then(cart => setCart(cart))
+            .catch(err => console.log(err))
+            .finally(() => setIsAdding(false));
+    }
 
     useEffect(() => {
         const fetchProduct = async () => {
             try {
-                const response = await fetch("http://localhost:5000/products/" + id);
-                setProduct(await response.json());
+                const response = await requests.products.details(id)
+                setProduct(response);
 
             } catch (error) {
                 console.log(error.message);
@@ -25,5 +40,12 @@ export default function ProductDetailsPage() {
 
     if (loading) return <Loading/>;
 
-    return <ProductItem product={product}/>;
+    if (!product) return <p>Product not found.</p>;
+
+    return <ProductItem
+        product={product}
+        handleAddItem={handleAddItem}
+        cartItem={cartItem}
+        isAdding={isAdding}
+    />;
 }
