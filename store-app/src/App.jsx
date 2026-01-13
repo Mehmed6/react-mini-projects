655cf4b5e99b1af3307504ffbef97603666f64b9
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2026-01-13 05:33:53 +0100

    Integrate routing and cart initialization in App
    Replaces static content with RouterProvider for navigation and initializes cart state on mount by fetching cart data via API. Enhances app structure and prepares for dynamic routing and cart management.

--- store-app/src/App.jsx
+++ store-app/src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
+import {RouterProvider} from "react-router";
+import {router} from "./routes/index.jsx";
+import {useEffect} from "react";
+import requests from "./api/apiClient.js";
+import {useCartContext} from "./context/CartContext.jsx";
 
 function App() {
 
-  return (
-    <>
-      <h1>App</h1>
-    </>
-  )
+    const {setCart} = useCartContext();
+
+    useEffect(() => {
+        requests.cart.get()
+            .then(cart => setCart(cart))
+            .catch(err => console.log(err))
+    }, []);
+
+  return <RouterProvider router={router}/>
 }
 
 export default App
