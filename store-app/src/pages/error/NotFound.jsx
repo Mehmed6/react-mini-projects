59f543a38ae56eedd59509adc177a464afada84e
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2026-01-13 05:32:03 +0100

    Add NotFound error page component
    Introduces a NotFound.jsx component displaying a 404 error message with a button to return to the homepage. Utilizes MUI components for styling and layout.

---
+++ store-app/src/pages/error/NotFound.jsx
@@ -0,0 +1,22 @@
+import {Alert, Button, Paper, Typography} from "@mui/material";
+import {Link} from "react-router";
+export default function NotFound() {
+
+    return (
+        <Paper sx={{ p: 3 }}>
+            <Typography variant="h4" gutterBottom>
+                Not Found Error
+            </Typography>
+            <Alert severity="error">Aradığınız kaynak bulunamadı.</Alert>
+            <Button
+                component={Link}
+                to="/"
+                variant="contained"
+                color="secondary"
+                sx={{ mt: 2 }}
+            >
+                Anasayfa
+            </Button>
+        </Paper>
+    );
+}
