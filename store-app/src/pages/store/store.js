67ce8d40a3c42937430515bb4d40de1ba7cc752c
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2026-01-15 04:47:09 +0100

    Add account slice to Redux store
    Imported and added accountSlice reducer to the Redux store configuration to manage account-related state.

--- store-app/src/pages/store/store.js
+++ store-app/src/pages/store/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {cartSliceReducer} from "../cart/cartSlice.js";
 import {catalogSlice} from "../catalog/catalogSlice.js";
+import {accountSlice} from "../account/accountSlice.js";
 
 export const store = configureStore({
     reducer: {
         cartStore: cartSliceReducer.reducer,
         catalogStore: catalogSlice.reducer,
+        accountStore: accountSlice.reducer,
     }
 });
