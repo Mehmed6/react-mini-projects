ea82774636fcf3f4bb09202649fe7b9fbcc02746
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2026-01-15 04:47:44 +0100

    Add user menu with logout and orders to Navbar
    Navbar now displays a dropdown menu for logged-in users, allowing access to orders and logout functionality. The menu uses MUI's Menu and MenuItem components, and integrates with Redux for logout and navigation.

--- store-app/src/components/Navbar.jsx
+++ store-app/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import {AppBar, Badge, Box, Button, IconButton, Toolbar} from "@mui/material";
+import {AppBar, Badge, Box, Button, IconButton, MenuItem, Toolbar, Menu} from "@mui/material";
+import { KeyboardArrowDown } from "@mui/icons-material";
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import {NavLink, Link} from "react-router";
-import {useSelector} from "react-redux";
+import {NavLink, Link, useNavigate} from "react-router";
+import {useDispatch, useSelector} from "react-redux";
+import {logout} from "../pages/account/accountSlice.js";
+import {useState} from "react";
 
 const links = [
     {title: "Home", to: "/" },
@@ -16,8 +19,29 @@ const authLinks = [
 
 export default function Navbar() {
 
+    const dispatch = useDispatch();
+    const {user} = useSelector(state => state.accountStore)
     const {cart} = useSelector(state => state.cartStore)
+    const navigate = useNavigate();
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
     const totalItems = cart?.cartItems.reduce((total, item) => total + item.product.quantity, 0);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    }
+
+    const handleClose = () => {
+      setAnchorEl(null);
+    }
+    const handleLogout = async () => {
+        handleClose()
+        dispatch(logout());
+        navigate("/login")
+    }
+
+
     return (
         <AppBar position="static" sx={{backgroundColor: "secondary.light"}}>
             <Toolbar>
@@ -37,11 +61,42 @@ export default function Navbar() {
                             <ShoppingCartIcon />
                         </Badge>
                     </IconButton>
-                    {authLinks.map(link => (
-                        <Button component={NavLink} to={link.to} color="inherit" key={link.title}>
-                            {link.title}
-                        </Button>
-                    ))}
+                    {
+                        user ? (
+                            <>
+                                <Button id="user-button"
+                                        onClick={handleClick}
+                                        endIcon={<KeyboardArrowDown/>}
+                                        color="inherit"
+                                >
+                                    {user.username}
+                                </Button>
+
+                                <Menu
+                                    id="user-menu"
+                                    anchorEl={anchorEl}
+                                    open={open}
+                                    onClose={handleClose}
+                                >
+                                    <MenuItem component={Link} to="/orders">
+                                        Order
+                                    </MenuItem>
+                                    <MenuItem onClick={handleLogout}>
+                                        Logout
+                                    </MenuItem>
+
+                                </Menu>
+                            </>
+                        ) : (
+                            <>
+                                {authLinks.map(link => (
+                                    <Button component={NavLink} to={link.to} color="inherit" key={link.title}>
+                                        {link.title}
+                                    </Button>
+                                ))}
+                            </>
+                        )
+                    }
                 </Box>
             </Toolbar>
         </AppBar>
