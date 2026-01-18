41daea71c8ffc04ce4c646c12ac430b2c2fdbce5
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2026-01-18 23:47:51 +0100

    Handle HTTP errors in MainLayout with navigation
    Added useEffect in MainLayout to set a global HTTP error handler using setOnHttpError. Navigates to appropriate error pages on 404 and 500 responses.

--- store-app/src/layouts/MainLayout.jsx
+++ store-app/src/layouts/MainLayout.jsx
@@ -1,8 +1,24 @@
-import {Outlet} from 'react-router';
+import {Outlet, useNavigate} from 'react-router';
 import Navbar from "../components/Navbar.jsx";
 import {Container} from "@mui/material";
 import {ToastContainer} from "react-toastify";
+import {useEffect} from "react";
+import {setOnHttpError} from "../api/apiClient.js";
 export default function MainLayout() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setOnHttpError(({status, data}) =>{
+            if (status === 404) navigate("/errors/not-found")
+            if (status === 500) navigate("/errors/server-error", {
+                state: {error: data, status}
+            });
+
+            return () => setOnHttpError(null);
+
+        })
+    }, [navigate]);
+
     return (
         <>
             <ToastContainer position="bottom-right" />
