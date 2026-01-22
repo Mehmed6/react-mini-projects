58aad7290d53b6cb68aee7eb41545a5d9a2dfc77
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2026-01-22 23:23:15 +0100

    Add AuthProvider and ToastContainer to app
    Wrapped the App component with AuthProvider in main.jsx to provide authentication context. Added ToastContainer to MainLayout for displaying toast notifications. Minor formatting cleanup in App.jsx.

--- login-page/src/App.jsx
+++ login-page/src/App.jsx
@@ -2,7 +2,6 @@ import {RouterProvider} from "react-router";
 import {router} from "@/routes/index.jsx";
 
 function App() {
-
     return <RouterProvider router={router}/>;
 }
 
--- login-page/src/layouts/MainLayout.jsx
+++ login-page/src/layouts/MainLayout.jsx
@@ -1,11 +1,18 @@
 import {Outlet} from "react-router";
+import {ToastContainer} from "react-toastify";
 
 export default function MainLayout() {
+
+
+
     return (
         <>
+            <ToastContainer  autoClose={2000} />
             //... you can add a header or navigation here if needed
             <Outlet/>
+
             //... you can add a footer here if needed
+
         </>
 
     )
